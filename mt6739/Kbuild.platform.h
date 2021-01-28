5629614b0081d4557a3cdc1fd28098bc4b0eff4e
weichuan.lin <weichuan.lin@mediatek.com>
2021-01-28 13:47:47 +0800

    [ALPS04452960] MET: androidQ migration for mt6739
    1. add mt6739 platform config
    2. due to do not get linux kernel version in Makefile,
       so change build flow to build source in build intermediates folder
    3. duet to only move .c/.h/.o/Kbuild/Makefile files to intermediates folder when build,
       so change Kbuild.platform.include to Kbuild.platform.h
    
    MTK-Commit-Id: 35c1ec7f6f34dfd304a2e2ec4b6176870d8e83de
    
    Change-Id: I72d06b802f537d664c43b1c193dcb10a0a85c8a5
    CR-Id: ALPS04452960
    Feature: [Module]MET (Mediatek Embedded Technology)

---
+++ mt6739/Kbuild.platform.h
@@ -0,0 +1,19 @@
+################################################################################
+# Include Path
+################################################################################
+MET_VCOREDVFS_INC := $(srctree)/drivers/misc/mediatek/base/power/include/vcorefs_v3
+MET_PTPOD_INC := $(srctree)/drivers/misc/mediatek/base/power/cpufreq_v2/src/mach/$(MTK_PLATFORM)/
+
+################################################################################
+# Feature Spec
+################################################################################
+SPMTWAM_VERSION := ap
+SPMTWAM_IDLE_SIGNAL_SUPPORT := single
+
+################################################################################
+# Feature On/Off
+################################################################################
+FEATURE_SSPM_EMI := n
+
+# ONDIEMET
+FEATURE_ONDIEMET := n
